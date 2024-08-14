f9662cfcbe868da1f9d146a3b37363c77f84926b
Kashish <kashyapkashish22@gmail.com>
2024-08-14 18:49:24 +0530

    Generic add fn for struct

--- add_generic/src/main.rs
+++ add_generic/src/main.rs
@@ -7,14 +7,16 @@ where T: Add<Output = T>{
 }
 
 #[derive(Debug)]
-struct Point{
-    x:u32,
-    y:u32,
+struct Point<T>{
+    x:T,
+    y:T,
 }
 
-impl Add for Point{
-    type Output= Point;
-    fn add(self, other: Point)-> Point{
+impl<T> Add for Point<T>
+where T:Add<Output = T>
+{
+    type Output= Point<T>;
+    fn add(self, other: Point<T>)-> Point<T>{
         Point { 
             x: self.x+ other.x, 
             y: self.y+ other.y
@@ -26,7 +28,6 @@ fn main(){
     let f1:f32=1.2;
     let f2:f32= 2.2;
     println!("Adding Float: {}", add(f1,f2));
-    
     let p1= Point{
         x:5, 
         y:0,
@@ -35,5 +36,16 @@ fn main(){
         x:0, 
         y:5,
     };
-    println!("Adding struct Point: {:?}", add(p1,p2));
+    println!("Adding struct Point Integers: {:?}", add(p1,p2));
+
+    let p1= Point{
+        x:5.5, 
+        y:0.0,
+    };
+    let p2= Point{
+        x:0.0,
+        y:5.5,
+    };
+    println!("Adding struct Point Float: {:?}", add(p1,p2));
+
 }
